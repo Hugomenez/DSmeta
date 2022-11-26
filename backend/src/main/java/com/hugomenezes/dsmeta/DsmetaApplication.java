9515d997e4c0886e139a789d11047aa2fd9f02c7
Hugo Menezes <hugofmenezes9@hotmail.com>
2022-11-26 20:34:50 -0300

    project creat

---
+++ backend/src/main/java/com/hugomenezes/dsmeta/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.hugomenezes.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
